75c6610154bb23c5728e92f80a819afdf41b7530
LaciaHax <130824935+LaciaHax@users.noreply.github.com>
2024-05-23 21:57:27 +0900

    Update fetch.py

--- libs/fetch.py
+++ libs/fetch.py
@@ -1,78 +1,75 @@
+import random
+import sys
 import json
 import websocket
-import time
+import requests
 
 def fetch_user_ids(token, guild, channel):
-    while True:
-        try:
-            ws = websocket.WebSocket()
-            ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
-            heartbeat_interval = json.loads(ws.recv())['d']['heartbeat_interval']
+    ws = websocket.WebSocket()
+    ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
+    heartbeat_interval = json.loads(ws.recv())['d']['heartbeat_interval']
 
-            ws.send(
-                json.dumps(
-                    {
-                        "op": 2,
-                        "d": {
-                            "token": token,
-                            "properties": {
-                                "$os": "windows",
-                                "$browser": "Discord",
-                                "$device": "desktop"
-                            }
-                        }
+    ws.send(
+        json.dumps(
+            {
+                "op": 2,
+                "d": {
+                    "token": token,
+                    "properties": {
+                        "$os": "windows",
+                        "$browser": "Discord",
+                        "$device": "desktop"
                     }
-                )
-            )
+                }
+            }
+        )
+    )
 
-            ws.send(
-                json.dumps(
-                    {
-                        "op": 14,
-                        "d": {
-                            "guild_id": guild,
-                            "typing": True,
-                            "threads": False,
-                            "activities": True,
-                            "members": [],
-                            "channels": {channel: [[0, 99]]}
-                        }
-                    }
-                )
-            )
+    ws.send(
+        json.dumps(
+            {
+                "op": 14,
+                "d": {
+                    "guild_id": guild,
+                    "typing": True,
+                    "threads": False,
+                    "activities": True,
+                    "members": [],
+                    "channels": {channel: [[0, 99]]}
+                }
+            }
+        )
+    )
 
-            received_data = ws.recv()
-            json_data = json.loads(received_data)
+    received_data = ws.recv()
+    json_data = json.loads(received_data)
 
-            guilds = json_data['d']['guilds']
+    guilds = json_data['d']['guilds']
 
-            for guild in guilds:
-                if guild["id"] == '956638414424387615':
-                    for member in guild["members"]:
-                        print(member["user"]["id"])
+    for guild in guilds:
+        if guild["id"] == '956638414424387615':
+            for member in guild["members"]:
+                print(member["user"]["id"])
 
-            ws.send(
-                json.dumps(
-                    {
-                        "op": 14,
-                        "d": {
-                            "guild_id": guild,
-                            "channels": {channel: [[0, 99], [100, 199]]}
-                        }
-                    }
-                )
-            )
+    ws.send(
+        json.dumps(
+            {
+                "op": 14,
+                "d": {
+                    "guild_id": guild,
+                    "channels": {channel: [[0, 99], [100, 199]]}
+                }
+            }
+        )
+    )
 
-            received_data = ws.recv()
-            json_data = json.loads(received_data)
+    received_data = ws.recv()
+    json_data = json.loads(received_data)
 
-            user_ids = []
-            for item in json_data['d']['ops'][0]['items']:
-                if 'member' in item:
-                    user_id = item['member']['user']['id']
-                    user_ids.append(user_id)
+    user_ids = []
+    for item in json_data['d']['ops'][0]['items']:
+        if 'member' in item:
+            user_id = item['member']['user']['id']
+            user_ids.append(user_id)
 
-            return user_ids
-        except Exception as e:
-            print(f"[-] An error occurred: {e}. Retrying in 10 seconds...")
-            time.sleep(10)
+    return user_ids
